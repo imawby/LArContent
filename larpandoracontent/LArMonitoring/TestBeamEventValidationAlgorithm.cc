f0c193e9e8f60c13c2fc8dce063a33338f069392
Isobel Mawby <Isobel.Mawby@warwick.ac.uk>
2020-04-01 13:16:39 +0100

    viktor hack

--- larpandoracontent/LArMonitoring/TestBeamEventValidationAlgorithm.cc
+++ larpandoracontent/LArMonitoring/TestBeamEventValidationAlgorithm.cc
@@ -140,10 +140,19 @@ void TestBeamEventValidationAlgorithm::ProcessOutput(const ValidationInfo &valid
         const int nTargetPrimaries(associatedMCPrimaries.size());
         ++mcPrimaryIndex;
         const CaloHitList &mcPrimaryHitList(validationInfo.GetAllMCParticleToHitsMap().at(pMCPrimary));
-
+	/*
         const int mcNuanceCode(LArMCParticleHelper::GetNuanceCode(LArMCParticleHelper::GetParentMCParticle(pMCPrimary)));
         const int isBeamParticle(LArMCParticleHelper::IsBeamParticle(pMCPrimary));
         const int isCosmicRay(LArMCParticleHelper::IsCosmicRay(pMCPrimary));
+	*/
+
+	//ISOBEL - HACK FOR VIKTOR'S EVENTS
+	const int mcNuanceCode(3000);
+        const int isBeamParticle(0);
+        const int isCosmicRay(1);
+
+
+
 #ifdef MONITORING
         const CartesianVector &targetVertex(LArMCParticleHelper::GetParentMCParticle(pMCPrimary)->GetVertex());
         const float targetVertexX(targetVertex.GetX()), targetVertexY(targetVertex.GetY()), targetVertexZ(targetVertex.GetZ());
@@ -308,7 +317,10 @@ void TestBeamEventValidationAlgorithm::ProcessOutput(const ValidationInfo &valid
 
         if (isBeamParticle || isCosmicRay)
         {
-            const LArInteractionTypeHelper::InteractionType interactionType(LArInteractionTypeHelper::GetInteractionType(associatedMCPrimaries));
+
+	    // ISOBEL - THIS IS A HACK FOR VIKTOR'S EVENTS
+	    //const LArInteractionTypeHelper::InteractionType interactionType(LArInteractionTypeHelper::GetInteractionType(associatedMCPrimaries));
+	  const LArInteractionTypeHelper::InteractionType interactionType(LArInteractionTypeHelper::InteractionType::COSMIC_RAY_MU);
 #ifdef MONITORING
             const int interactionTypeInt(static_cast<int>(interactionType));
 #endif
