0aecd58431f2651ed1d9e0f714454d06f7446ef9
Isobel Mawby <Isobel.Mawby@warwick.ac.uk>
2020-05-12 22:18:00 +0100

    map changes

--- larpandoracontent/LArControlFlow/StitchingCosmicRayMergingTool.cc
+++ larpandoracontent/LArControlFlow/StitchingCosmicRayMergingTool.cc
@@ -585,7 +585,6 @@ void StitchingCosmicRayMergingTool::StitchPfos(const MasterAlgorithm *const pAlg
         const float tpcBoundaryCenterX(LArStitchingHelper::GetTPCBoundaryCenterX(*stitchedLArTPCs.first, *stitchedLArTPCs.second));
         bool isCPAStitch(stitchedLArTPCs.first->GetCenterX() < tpcBoundaryCenterX ? !stitchedLArTPCs.first->IsDriftInPositiveX() : !stitchedLArTPCs.second->IsDriftInPositiveX());
 
-        float x0ToAdd(0);
         if (!m_useXcoordinate || m_alwaysApplyT0Calculation)
         {
             PfoToPointingVertexMap pfoToPointingVertexMap;
@@ -603,8 +602,6 @@ void StitchingCosmicRayMergingTool::StitchPfos(const MasterAlgorithm *const pAlg
             {
                 const float t0Sign(isCPAStitch ? -1.f : 1.f);
 
-                x0ToAdd = x0 * (isCPAStitch ? -1.f : 1.f);
-                
                 object_creation::ParticleFlowObject::Metadata metadata;
                 metadata.m_propertiesToAdd["X0"] = x0 * t0Sign;
 
@@ -635,8 +632,8 @@ void StitchingCosmicRayMergingTool::StitchPfos(const MasterAlgorithm *const pAlg
         if (m_writeToTree)
         {
             int matchesSize(pfoVector.size());
-            const PropertiesMap &propertiesMap(pPfoToEnlarge->GetPropertiesMap());
-            PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), m_treeName, "APA_X0", propertiesMap.at("X0")));
+            const PropertiesMap &propertiesMap(pSelectedPfoToEnlarge->GetPropertiesMap());
+	    PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), m_treeName, "APA_X0", propertiesMap.at("X0")));
             PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), m_treeName, "Matches", matchesSize));
             PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), m_treeName, "EventNumber", m_eventNumber));
             PANDORA_MONITORING_API(FillTree(this->GetPandora(), m_treeName));
